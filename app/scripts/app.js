2eb9558bc878e2df9063e8d8f136620a41188b4b
Antonio Fazari <antonio.fazari88@gmail.com>
2014-03-21 12:34:38 +0000

    Initial yeoman angular install

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('roundzeroApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
