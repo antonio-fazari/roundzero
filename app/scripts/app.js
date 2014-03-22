05267f737116a2800a404c809887f452a16e7cc9
freddybushboy <fjcparke@gmail.com>
2014-03-22 13:22:56 +0000

    add the first few prototype page - login and group creation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,8 +12,24 @@ angular.module('roundzeroApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/account', {
-        templateUrl: 'views/account.html',
+      .when('/sign-in', {
+        templateUrl: 'views/sign-in.html',
+        controller: 'MainCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'MainCtrl'
+      })
+      .when('/add-group', {
+        templateUrl: 'views/add-group.html',
+        controller: 'MainCtrl'
+      })
+      .when('/add-people', {
+        templateUrl: 'views/add-people.html',
+        controller: 'MainCtrl'
+      })
+      .when('/group', {
+        templateUrl: 'views/group.html',
         controller: 'MainCtrl'
       })
       .otherwise({
