886a5ece8c06d2ee1a86cb229fa78cb8b5dba4d2
freddybushboy <fjcparke@gmail.com>
2014-03-27 23:23:31 +0000

    messing around with angular, some test functionality on the add group page mainly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,22 @@
 
 angular.module('roundzeroApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    // Testing.
+    $scope.testPeople = [
+      {name: 'Fred', id: 1, val: -2},
+      {name: 'Tony', id: 2, val: 2},
+      {name: 'Graham', id: 3, val: 1}
     ];
+
+    // Messing around on add-group page.
+    $scope.newPeople = [];
+    var id = 1;
+    $scope.addPerson = function (formNewPerson) {
+      $scope.newPeople.push({
+        name: formNewPerson,
+        id: id
+      });
+      id++;
+    };
+
   });
