6232adb3c97161d837c7a21a7fad83ea805e6b88
freddybushboy <fjcparke@gmail.com>
2014-03-28 21:11:47 +0000

    angular chanes - add header partial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,27 +3,7 @@
 angular.module('roundzeroApp')
   .controller('MainCtrl', function ($scope, AuthService) {
     $scope.$watch(AuthService.isLoggedIn, function (isLoggedIn) {
-      $scope.isLoggedIn = isLoggedIn;
-      $scope.currentUser = AuthService.currentUser();
+        $scope.isLoggedIn = isLoggedIn;
+        $scope.currentUser = AuthService.currentUser();
     });
-
-    // Testing.
-    $scope.testPeople = [
-      {name: 'Fred', id: 1, val: -2},
-      {name: 'Tony', id: 2, val: 2},
-      {name: 'Graham', id: 3, val: 1}
-    ];
-
-    // Messing around on add-group page.
-    $scope.newPeople = [];
-    var id = 1;
-    $scope.addPerson = function (formNewPerson) {
-      if (formNewPerson) {
-        $scope.newPeople.push({
-          name: formNewPerson,
-          id: id
-        });
-        id++;
-      }
-    };
-  });
+});
