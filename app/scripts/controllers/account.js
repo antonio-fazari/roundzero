5097c9f758e3324fd788ff00d60551617d870468
Graham Bates <info@grahambates.com>
2014-03-31 23:57:18 +0100

    Added account edit functionality.

--- app/scripts/controllers/account.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('roundzeroApp')
-    .controller('AccountCtrl', ['$scope', 'AuthService', 'UserService',
-        function ($scope, AuthService, UserService) {
-            $scope.user = UserService.get({id: AuthService.userId});
-        }
-    ]);
