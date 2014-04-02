c16793bed791e8ca23abae5c777d5802e28459b3
Antonio Fazari <antonio.fazari88@gmail.com>
2014-04-02 18:20:00 +0100

    Added user membership of group creator

--- app/scripts/controllers/addGroup.js
+++ app/scripts/controllers/addGroup.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('roundzeroApp')
-    .controller('AddGroupCtrl', ['$scope', '$http', '$location', 'AuthService', 'GroupService', 'MembershipService',
-        function ($scope, $http, $location, AuthService, GroupService, MembershipService) {
+    .controller('AddGroupCtrl', ['$scope', '$http', '$location', 'storage', 'AuthService', 'GroupService', 'MembershipService',
+        function ($scope, $http, $location, storage, AuthService, GroupService, MembershipService) {
             $scope.submitted = false;
             $scope.loading = false;
             $scope.error = null;
@@ -19,27 +19,28 @@ angular.module('roundzeroApp')
                 if (!$scope.form.$invalid) {
                     $scope.loading = true;
                     $scope.group.$save(
-                        function success() {
+                        function success(response) {
                             $scope.loading = false;
                             $scope.error = null;
-                            // // Add member that created the group.
-                            // var member = {
-                            //     groupId: 2,
-                            //     userId: AuthService.userId
-                            // }
-                            // $http.post('http://api.roundzeroapp.com/v1/memberships', member)
-                            //     .success(function(response) {
-                            //         $location.path('/account');
-                            //     })
-                            //     .error(function(response) {
-                            //         $scope.loading = false;
+                            // Add member that created the group.
+                            var membership = new MembershipService();
+                            membership.groupId = response.id;
+                            membership.userId = AuthService.userId;
 
-                            //         if (response.error) {
-                            //             $scope.error = response.error;
-                            //         } else {
-                            //             $scope.error = 'There was an error logging in. Please try later.';
-                            //         }
-                            //     });
+                            membership.$save(
+                                function success(response) {
+                                    $location.path('/account');
+                                },
+                                function error(response) {
+                                    $scope.loading = false;
+
+                                    if (response.error) {
+                                        $scope.error = response.error;
+                                    } else {
+                                        $scope.error = 'There was an error logging in. Please try later.';
+                                    }
+                                }
+                            );
 
                             $location.path('/account');
                         },
