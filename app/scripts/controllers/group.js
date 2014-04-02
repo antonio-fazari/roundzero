1ddd54f109d005e5276faf3b686808d533f1f165
Antonio <antonio.fazari88@gmail.com>
2014-04-03 00:40:43 +0100

    Added user object to the scope to list the users groups in the dropdown menu

--- app/scripts/controllers/group.js
+++ app/scripts/controllers/group.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('roundzeroApp')
-    .controller('GroupCtrl', ['$scope', '$routeParams', '$location', 'GroupService',
-        function ($scope, $routeParams, $location, GroupService) {
-            // Get all group details
+    .controller('GroupCtrl', ['$scope', '$routeParams', 'AuthService', 'GroupService', 'UserService',
+        function ($scope, $routeParams, AuthService, GroupService, UserService) {
+            // Get all group details.
             $scope.group = GroupService.get({id: $routeParams.groupId});
+            // TODO: Fix API so group object comes along with user memberships.
+            $scope.user = UserService.get({id: AuthService.userId});
         }
     ]);
